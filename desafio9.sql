535e0c41f45031334981f0b2c171e7b168c4865e
ViniciusMossi <vini_agg@hotmail.com>
2021-11-04 22:22:49 -0400

    verifica req 9

---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN nome_artista VARCHAR(100))
+BEGIN
+SELECT
+    artist.artista AS `artista`,
+    album.album AS `album`
+FROM
+SpotifyClone.artistas AS artist
+INNER JOIN SpotifyClone.albuns AS album ON artist.artista_id = album.artista_id
+WHERE
+    artista = nome_artista;
+END $$
+
+DELIMITER ;
