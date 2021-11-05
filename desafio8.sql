4188833ce08fd911699bd9e7894fb2af10e87093
ViniciusMossi <vini_agg@hotmail.com>
2021-11-04 22:01:30 -0400

    implementa req 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.usuarios
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.seguindo
+  WHERE OLD.usuario_id = usuario_id;
+  DELETE FROM SpotifyClone.reproducoes
+  WHERE OLD.usuario_id = usuario_id;
+END $$
+** ainda não comitei
